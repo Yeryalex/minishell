709540b5d242bcc68915005741f72eb10db31929
RubénBM <weber.atas@gmail.com>
2024-11-20 12:06:10 +0100

    minishell.h estructura para pensar funciones

--- inc/minishell.h
+++ inc/minishell.h
@@ -3,20 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yrodrigu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rbuitrag <rbuitrag@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 17:32:28 by yrodrigu          #+#    #+#             */
-/*   Updated: 2024/11/19 21:38:42 by yrodrigu         ###   ########.fr       */
+/*   Updated: 2024/11/20 12:05:04 by rbuitrag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 # define CYAN "\033[96m"
 # define GREEN "\033[92m"
@@ -26,5 +26,28 @@
 # define BLUE "\033[94m"
 # define GRAY "\033[90m"
 
+/*          MAIN FUNCTIONS         */
+
+/*          LEXER FUNCTIONS         */
+
+/*          STRUCT FUNCTIONS         */
+
+/*          REDIR FUNCTIONS         */
+
+/*          BUILTINS FUNCTIONS         */
+
+/*          SIGNAL FUNCTIONS         */
+
+/*          ENV FUNCTIONS         */
+
+/*          EXPORT FUNCTIONS         */
+
+/*          EXPAND FUNCTIONS         */
+
+/*          EXECUTOR FUNCTIONS         */
+
+/*          AUXILIARS FUNCTIONS         */
+
+/*          FREE FUNCTIONS         */
 
 #endif
