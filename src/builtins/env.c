aec065d3dba2073a09a1f2f5b9919d90194a3d0b
Yery Rodriguez <128627025+Yeryalex@users.noreply.github.com>
2025-01-25 12:20:39 +0100

    Add condition for values in env

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: yrodrigu <yrodrigu@student.42barcelo>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 11:04:43 by yrodrigu          #+#    #+#             */
-/*   Updated: 2025/01/25 11:24:38 by yrodrigu         ###   ########.fr       */
+/*   Updated: 2025/01/25 12:19:40 by yrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../inc/minishell.h"
@@ -19,10 +19,13 @@ int	ft_env(t_utils *utils, int fd)
 	temp = utils->environ;
 	while (temp)
 	{
-		ft_putstr_fd(temp->key, fd);
-		ft_putchar_fd('=', fd);
-		ft_putstr_fd(temp->value, fd);
-		ft_putchar_fd('\n', fd);
+		if (temp->value)
+		{
+			ft_putstr_fd(temp->key, fd);
+			ft_putchar_fd('=', fd);
+			ft_putstr_fd(temp->value, fd);
+			ft_putchar_fd('\n', fd);
+		}
 		temp = temp->next;
 	}
 	return (0);
