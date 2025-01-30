81fd79666c956ebebf755c0c970f0fe047c76c6c
Yery Rodriguez <128627025+Yeryalex@users.noreply.github.com>
2025-01-30 13:27:49 +0100

    Export function working properly, Working on the += to concat strings if existing

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: yrodrigu <yrodrigu@student.42barcelo>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 12:44:57 by yrodrigu          #+#    #+#             */
-/*   Updated: 2025/01/29 13:48:27 by yrodrigu         ###   ########.fr       */
+/*   Updated: 2025/01/30 13:21:02 by yrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../inc/minishell.h"
@@ -99,63 +99,109 @@ t_env	*ft_find_key_env(t_env *env, char *key_value)
 	return (NULL);
 }
 
+int	ft_abletojoin(char *str)
+{
+	int i;
+
+	i = ft_strlen(str) - 1;
+	if (str[i] == '+' && str[i - 1] != '+')
+			return (1);
+	return (0);
+}
+
 t_env *ft_add_node_env(char **cmd_array, t_env *env)
 {
 	int i;
 	t_env	*node;
 	t_env	*temp;
 	char	**key_value;
+	int		concat_flag = 0;
 	
 	i = 1;
 	while (cmd_array && cmd_array[i])
 	{
+		while (cmd_array[i] && cmd_array[i][0] == '=')
+		{
+			printf("minishell: export: `%s': not a valid identifier\n", cmd_array[i]);
+			i++;
+		}
+
+		if (!cmd_array[i])
+			return (env);
 		node = (t_env *)malloc(sizeof(t_env));
 		node->next = NULL;
 		
 		key_value = ft_split(cmd_array[i], '=');
 		int	add_equals = ft_exist_char(cmd_array[i]);
 		
-		t_env *node_with_key = ft_find_key_env(env, key_value[0]);
-		if (add_equals)
+		if (ft_abletojoin(key_value[0]))
 		{
+			char *plus_join;
+
+			plus_join = key_value[0];
+			key_value[0] = ft_strtrim(key_value[0], "+");
+			if (key_value[1])
+				concat_flag = 1;
+			free(plus_join);
+		}
 		
-		//	key_value = ft_split(cmd_array[i], '=');
-		//	t_env *temp = ft_find_key_env(env, key_value[0]);
-			if (!node_with_key)
+		t_env *node_with_key = ft_find_key_env(env, key_value[0]);
+		printf("%i\n", concat_flag);
+
+		char	*final_value;
+
+		if (key_value[1])
+			final_value = ft_strtrim(key_value[1], "\"\'");
+
+		if (node_with_key && concat_flag)
+		{
+			char *value_join;
+			value_join = key_value[1];
+			
+			if (node_with_key->value)
 			{
-				node->key = key_value[0];
-				if (add_equals == 1)
-				{
-					if (key_value[1])
-						node->value = key_value[1];
-					else
-						node->value = NULL;
-				}
-				else 
-					node->value = ft_strjoin(ft_add_equals(add_equals - 1), key_value[1]);
+				key_value[1] = ft_strjoin(node_with_key->value, key_value[1]);
+				free(value_join);
 			}
-			else
+		/*	else
+			{
+				node_with_key->value = key_value[1];
+				free(value_join);
+			}*/
+		}
+		if (node_with_key)
+		{
+			if (add_equals)
 			{
 				if (node_with_key->value)
-					free(node_with_key->value);
-				if (key_value[1])
-					node_with_key->value = ft_strdup(key_value[1]);
-				else
-					key_value = NULL;
-				break ;
+     				free(node_with_key->value);
+			
+ 			if (final_value)
+     			node_with_key->value = final_value;
+ 			else
+     			node_with_key->value = NULL;
 			}
+			i++;
+			continue ;
 		}
-		else if (!add_equals)
+		if (add_equals)
 		{
-			if (!node_with_key)
+			node->key = key_value[0];
+			if (add_equals == 1)
 			{
-				node_with_key->key = ft_strdup(cmd_array[i]);
-				node_with_key->value = NULL;
+    			if (final_value)
+        			node->value = final_value;
+    			else
+        			node->value = NULL;
 			}
-			else
-			{
-				node_with_key->key = ft_strdup(cmd_array[i]);
-			}
+			else 
+    			node->value = ft_strjoin(ft_add_equals(add_equals - 1), final_value);
+		}
+		else
+		{
+			
+			node->key = key_value[0];
+			node->value = final_value;
 		}
         temp = env;
         while (temp->next)
